571919388f846486f00030b838fb5ed30cba0d82
Chelal <31129985+xfvvvv@users.noreply.github.com>
2021-08-02 16:24:04 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -15,4 +15,9 @@
 
 # Add a feed source
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
-echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+#echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+echo 'src-git packages https://git.openwrt.org/feed/packages.git;openwrt-21.02' >>feeds.conf.default
+echo 'src-git luci https://git.openwrt.org/project/luci.git;openwrt-21.02' >>feeds.conf.default
+echo 'src-git routing https://git.openwrt.org/feed/routing.git;openwrt-21.02' >>feeds.conf.default
+#echo 'src-git telephony https://git.openwrt.org/feed/telephony.git;openwrt-21.02' >>feeds.conf.default
+echo 'src-git custom https://github.com/kiddin9/openwrt-packages.git' >>feeds.conf.default
